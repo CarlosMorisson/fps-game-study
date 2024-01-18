397ea3362a69aae6cdcc3867ab3c617470d64846
carlos <carlos.lescanomorisson@gmail.com>
2024-01-17 23:47:33 -0400

    Shoot and Reload Animations

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,15 +10,23 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     private int _totalBullets = 30;
     [SerializeField]
-    private int _bulletsLeft;
+    private int _bulletsLeft=100;
+    [SerializeField]
+    private int _currentBullets;
     //
+    private Animator anim;
     [SerializeField]
     private float _fireRate = 0.1f;
     [SerializeField]
     private float _fireTimer;
+    [SerializeField]
+    private ParticleSystem _fireEffect;
+    public Transform FirePoint;
+    private bool _isReloading;
     void Start()
     {
-        
+        _currentBullets = _totalBullets;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,19 +34,59 @@ public class Weapon : MonoBehaviour
     {
         if (Input.GetButton("Fire1"))
         {
-            Fire();
+            if (_currentBullets > 0)
+                Fire();
+            else if (_bulletsLeft > 0)
+                DoReload();
         }
         if (_fireTimer < _fireRate)
         {
             _fireTimer += Time.deltaTime;
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if(_currentBullets<_totalBullets && _bulletsLeft > 0)
+            {
+                DoReload();
+            }
+        }
     }
     private void Fire()
     {
-        if (_fireTimer < _fireRate)
+        if (_fireTimer < _fireRate || _isReloading || _currentBullets<0)
+            return;
+        
+        RaycastHit hit;
+        if(Physics.Raycast(FirePoint.position, FirePoint.transform.forward, out hit, _range))
+        {
+
+        }
+        anim.CrossFadeInFixedTime("Fire", 0.02f);
+        _fireEffect.Play();
+        _currentBullets--;
+        _fireTimer = 0f;
+    }
+    private void FixedUpdate()
+    {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+        _isReloading = info.IsName("Reload");
+    }
+    private void DoReload()
+    {
+        if (_isReloading)
+            return;
+        anim.CrossFadeInFixedTime("Reload", 0.02f);
+    }
+    public void Reload()
+    {
+        if (_bulletsLeft <= 0)
         {
             return;
         }
-        _fireTimer = 0f;
+        int bulletsToLoad = _totalBullets - _currentBullets;
+        int bulletsToDeduct = (_bulletsLeft>=bulletsToLoad) ? bulletsToLoad : _bulletsLeft;
+
+        _bulletsLeft -= bulletsToDeduct;
+        _currentBullets += bulletsToDeduct;
     }
 }
