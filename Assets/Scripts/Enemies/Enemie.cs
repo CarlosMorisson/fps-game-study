2759ea3da13aae13baa06c08abcc3da19264727a
carlos <carlos.lescanomorisson@gmail.com>
2024-02-19 23:38:50 -0400

    adicionado nav mesh agent

--- Assets/Scripts/Enemies/Enemie.cs
+++ Assets/Scripts/Enemies/Enemie.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemie : MonoBehaviour
 {
@@ -8,19 +9,43 @@ public class Enemie : MonoBehaviour
     public float Life;
     public float Speed;
     public Transform Target;
-    
+    public NavMeshAgent Agent;
+    public List<Rigidbody> RagDollComponents;
     void Start()
     {
         instance = this;
+        ActiveRagdoll();
+
+
     }
 
     public void ReceiveDamage(float damage)
     {
         Life -= damage;
-
+        DeactiveRagdoll();
+    }
+    public void ActiveRagdoll()
+    {
+        for (int i = 0; i < RagDollComponents.Count; i++)
+        {
+            RagDollComponents[i].useGravity = true;
+            RagDollComponents[i].isKinematic = false;
+        }
+    }
+    public void DeactiveRagdoll()
+    {
+        for (int i = 0; i < RagDollComponents.Count; i++)
+        {
+            RagDollComponents[i].useGravity = false;
+            RagDollComponents[i].isKinematic = true;
+        }
     }
     void Update()
     {
-        
+        FallowPlayer();
+    }
+    public virtual void FallowPlayer()
+    {
+        Agent.SetDestination(Target.position);
     }
 }
--- Assets/Scripts/Enemies/EnemieMember.cs
+++ Assets/Scripts/Enemies/EnemieMember.cs
@@ -19,4 +19,5 @@ public class EnemieMember : MonoBehaviour
         }
         Enemie.instance.ReceiveDamage(damage);
     }
+
 }
